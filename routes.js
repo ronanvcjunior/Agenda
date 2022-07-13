56ea4e8f02d1170c7118cce3b84e13dfe3608437
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2022-07-13 14:38:48 -0300

    static index

--- routes.js
+++ routes.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const route = express.Router()
 
+const homeController = require('./src/controllers/homeController')
+
+// Rotas da index
+route.get('/', homeController.index)
+
 module.exports = route
---
+++ src/controllers/homeController.js
@@ -0,0 +1,5 @@
+exports.index = (req, res) => {
+  return res.render('index', {
+    title: 'Homepage'
+  })
+}
